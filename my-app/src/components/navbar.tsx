1f8c865069c718c12739b9bd7640280d1f25784a
SantiBerri <santiberri15@gmail.com>
2024-07-30 18:49:14 -0300

    push

--- my-app/src/components/navbar.tsx
+++ my-app/src/components/navbar.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import Social from './social';
 import Socialgit from './socialgit';
-
+import Link from 'next/link';
+import cv from '@/../public/cv.pdf';
 
 const Navbar = () => {
   return (
     <div className="h-20 bg-transparent sticky top-0 w-full z-50 px-4">
       <div className="flex items-center justify-center">
         <Socialgit />
-        <button className="mt-5 px-4 py-2  text-white rounded">Proyectos</button>
-        <button className="mt-5 px-4 py-2  text-white rounded">Curriculum</button>
-        <button className="mt-5 px-4 py-2  text-white rounded">Contacto</button>
+        <button className="mt-5 px-4 py-2 text-white rounded">Proyectos</button>
+        <a 
+          href={cv} 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="mt-5 px-4 py-2 text-white rounded bg-gradient-to-r from-gray-800 to-black border border-gray-600 hover:scale-105 duration-200 hover:text-gray-500 hover:border-gray-800 hover:from-black hover:to-gray-900"
+        >
+          Currículum
+        </a>
+        <button className="mt-5 px-4 py-2 text-white rounded">Contacto</button>
         <Social />
       </div>
     </div>
